f71a56cb34402c34dcddfdf8a924e1aaff9b34cb
Utsav <utsavpabari@gmail.com>
2023-03-01 16:47:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import MacbookAir1 from "./component/MacbookAir1";
+
+function App() {
+  return (
+    <div className="main">
+      <MacbookAir1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/MacbookAir1.js
@@ -0,0 +1,222 @@
+import React from "react";
+
+const MacbookAir1 = () => {
+  return (
+    <div className="row">
+      <div className="col-md-1">
+        <div className="inner">
+          <div className="logo">
+            <img src={require("../images/Vector771Stroke.png")} alt="" />
+          </div>
+
+          <div className="cmn">
+            <img src={require("../images/+.png")} alt="" />
+            <div className="abt">
+              <p>new</p>
+            </div>
+
+            <img src={require("../images/image897.png")} alt="" />
+            <div className="abt">
+              <p>patient</p>
+            </div>
+
+            <img src={require("../images/image898.png")} alt="" />
+            <div className="abt">
+              <p>folder</p>
+            </div>
+
+            <img src={require("../images/image899.png")} alt="" />
+            <div className="abt">
+              <p>upload</p>
+            </div>
+
+            <img src={require("../images/image900.png")} alt="" />
+            <div className="abt">
+              <p>report</p>
+            </div>
+
+            <img src={require("../images/image901.png")} alt="" />
+            <div className="abt">
+              <p>setting</p>
+            </div>
+
+            <img src={require("../images/image902.png")} alt="" />
+            <div className="abt">
+              <p>logout</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-3">
+        <div className="row">
+          <div className="sec2">
+            <div className="for-image">
+              <img src={require("../images/Ellipse1238.png")} alt="" />
+            </div>
+            <div className="diane">
+              <h1>diane cooper</h1>
+              <p>diane.cooper@example.com</p>
+            </div>
+            <div className="new">
+              <div className="for-number">
+                <h1>15</h1>
+                <p>Past</p>
+              </div>
+              <div className="for-border"></div>
+              <div className="num2">
+                <h1>02</h1>
+                <p>Upcoming</p>
+              </div>
+            </div>
+            <div className="brd">
+              <p>Send Messages</p>
+            </div>
+            <div className="files">
+              <p>Files / Documents</p>
+            </div>
+            <div className="checkup">
+              <img src={require("../images/path3683.png")} alt="" />
+              <p>Check Up Results.pdf</p>
+            </div>
+            <div className="checkup">
+              <img src={require("../images/path3683.png")} alt="" />
+              <p>Check Up Results.pdf</p>
+            </div>
+            <div className="checkup">
+              <img src={require("../images/path3683.png")} alt="" />
+              <p>Check Up Results.pdf</p>
+            </div>
+            <div className="checkup">
+              <img src={require("../images/path3683.png")} alt="" />
+              <p>Check Up Results.pdf</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-8">
+        <div className="row">
+          <div className="sec3">
+            <div className="sec3-detail">
+              <p>Gender</p>
+              <h6>Female</h6>
+              <div className="brdr-btm"></div>
+            </div>
+            <div className="sec3-detail">
+              <p>Birthday</p>
+              <h6>Feb 24th, 1997</h6>
+              <div className="brdr-btm"></div>
+            </div>
+            <div className="sec3-detail">
+              <p>Phone Number</p>
+              <h6>(239) 555 -0108</h6>
+              <div className="brdr-btm"></div>
+            </div>
+            <div className="sec3-detail">
+              <p>Registered Date</p>
+              <h6>Feb 24th, 1997</h6>
+              <div className="brdr-btm"></div>
+            </div>
+          </div>
+          <div className="sec4">
+            <div className="sec3-detail">
+              <p>Street Address</p>
+              <h6>JL. Diponegoro No. 21</h6>
+              <div className="brdr-btm"></div>
+            </div>
+            <div className="sec3-detail">
+              <p>City</p>
+              <h6>Cilacap</h6>
+              <div className="brdr-btm"></div>
+            </div>
+            <div className="sec3-detail">
+              <p>Zip Code</p>
+              <h6>655849</h6>
+              <div className="brdr-btm"></div>
+            </div>
+            <div className="sec3-detail">
+              <p>Member Status</p>
+              <h6>Active Member</h6>
+              <div className="brdr-btm"></div>
+            </div>
+          </div>
+          <div className="frame">
+            <div className="upcoming">
+              <p>Upcoming Appointments</p>
+            </div>
+            <div className="past">
+              <p>Past Appointments</p>
+            </div>
+            <div className="medical">
+              <p>Medical Records</p>
+            </div>
+          </div>
+          <div className="frame1">
+            <div className="frame1-detail">
+              <div className="rt-canal">
+                <p>Root Canal Treatment</p>
+              </div>
+              <div className="previous">
+                <p>Show Previous Treatments</p>
+              </div>
+            </div>
+            <div className="rect">
+              <div className="rect-1 ">
+                <div className="rect-1h1">
+                  <h1>26 Nov ‘19</h1>
+                  <p>09.00 - 10.00</p>
+                </div>
+                <div className="for-border"></div>
+                <div className="treatment">
+                  <h1>Treatment</h1>
+                  <p>Open Access</p>
+                </div>
+                <div className="for-border"></div>
+                <div className="treatment">
+                  <h1>Dentist</h1>
+                  <p>Drg. Adam H.</p>
+                </div>
+                <div className="treatment">
+                  <h1>Nurse</h1>
+                  <p>Jessicamila</p>
+                </div>
+                <div className="note">
+                  <img src={require("../images/path3683.png")} alt="" />
+
+                  <p>Note</p>
+                </div>
+              </div>
+
+              <div className="rect-2">
+                <div className="rect-1h1">
+                  <h1>12 Nov ‘19</h1>
+                  <p>09.00 - 10.00</p>
+                </div>
+                <div className="for-border"></div>
+                <div className="treatment">
+                  <h1>Treatment</h1>
+                  <p>Root Canal</p>
+                </div>
+                <div className="for-border"></div>
+                <div className="treatment">
+                  <h1>Dentist</h1>
+                  <p>Drg. Adam H.</p>
+                </div>
+                <div className="treatment">
+                  <h1>Nurse</h1>
+                  <p>Jessicamila</p>
+                </div>
+                <div className="note">
+                  <img src={require("../images/path3683.png")} alt="" />
+
+                  <p>Note</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MacbookAir1;
